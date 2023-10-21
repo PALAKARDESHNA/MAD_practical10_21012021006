76d8406826c5a96e97eb118d4d8fcd8ca5dab24b
student <student@gnu.ac.in>
2023-10-21 11:11:32 +0530

    practical 10

--- app/src/main/java/com/example/mad_practical10_21012021006/contactAdapter.kt
+++ app/src/main/java/com/example/mad_practical10_21012021006/contactAdapter.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
-class contactAdapter(val context: Context, val array:ArrayList<contect>):ArrayAdapter<contect>(context,0,array){
+class contactAdapter( context: Context, val array:ArrayList<contect>):ArrayAdapter<contect>(context,0,array){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.listviewlayout,parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.listviewlayout,parent,false)
+        view.findViewById<TextView>(R.id.textView2).text = array.get(position).name
+        view.findViewById<TextView>(R.id.textView3).text = array.get(position).address
+        view.findViewById<TextView>(R.id.textView4).text = array.get(position).emailid
 
         return view
     }
